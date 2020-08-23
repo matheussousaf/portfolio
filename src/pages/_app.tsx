c35317fa98db96d79bb5abb5e018cc8414da3bae
Matheus de Sousa <matheussousaf7@gmail.com>
2020-08-23 20:16:01 -0300

    :bug: unused variables

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import useDarkMode from "use-dark-mode";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "../styles/themes";
